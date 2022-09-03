379dfdfb25530b866bb2c9544f249ff5207d67ec
randompast <matteroflight@gmail.com>
2022-09-03 16:47:34 -0400

    added detect_labels functionality and joke checking

---
+++ example_output.py
@@ -0,0 +1,6 @@
+example_aws_output = [
+        {'Name': 'Broccoli', 'Confidence': 99.84778594970703, 'Instances': [], 'Parents': [{'Name': 'Vegetable'}, {'Name': 'Plant'}, {'Name': 'Food'}]}
+        ,{'Name': 'Plant', 'Confidence': 99.84778594970703, 'Instances': [], 'Parents': []}
+        ,{'Name': 'Vegetable', 'Confidence': 99.84778594970703, 'Instances': [], 'Parents': [{'Name': 'Plant'}, {'Name': 'Food'}]}
+        ,{'Name': 'Food', 'Confidence': 99.84778594970703, 'Instances': [], 'Parents': []}
+    ]
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from json import detect_encoding
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.camera import Camera
+from example_output import example_aws_output
+import boto3
+import random
+
 class ScannerApp(App):
     def build(self):
         self.cam = Camera()
@@ -10,22 +15,47 @@ class ScannerApp(App):
         self.cam.play = True
         layout = BoxLayout()
         layout.add_widget(self.cam)
-        self.make_button(layout, "take picture", self.take_pic)
-        self.make_button(layout, "Should I Eat It", self.detect_labels)
+        side_layout = BoxLayout()
+        side_layout.orientation = "vertical"
+        layout.add_widget(side_layout)
+        self.make_widget(side_layout, Button, "take picture", fun=self.take_pic)
+        self.make_widget(side_layout, Button, "Should I Eat It", fun=self.detect_labels)
+        self.results = self.make_widget(side_layout, Label, "results")
+        self.results.text_size = [self.results.width, None] 
         return layout
 
     def take_pic(self, *args):
         self.cam.export_to_png("./scan.png")
 
+    def parse_responses(self, responses):
+        responses = [i["Name"] for i in example_aws_output]
+        possible_responses = ["Probably don't eat it", "You might glow after", "It might not kill you?"]
+        # for res in responses:
+        #     for key in joke_dictionary:
+        #         if(res in joke_dictionary[key]):
+        #             possible_responses += [key]
+        r = random.randint(0, len(possible_responses)-1)
+        return possible_responses[ r ]
+
     def detect_labels(self, *args):
         print("detecting labels")
+        if not example_aws_output:
+            client=boto3.client('rekognition')
+            with open("./scan.png", 'rb') as image:
+                response = client.detect_labels(Image={'Bytes': image.read()})
+            print(response["Labels"])
+            response = self.parse_responses(response["Labels"])      
+            self.results.text =  response
+        else:
+            response = self.parse_responses(example_aws_output)      
+            self.results.text =  response
 
-        #Should I Eat It Button
-    def make_button(self, layout, text, fun):
-        button = Button(text=text)
-        button.size_hint = (.5,.2)
-        button.pos_hint = {'x': .30, 'y': .30}
-        button.bind(on_press=fun)
-        layout.add_widget(button)
+    def make_widget(self, layout, Widget, text, fun=None):
+        widget = Widget(text=text)
+        widget.size_hint = (.5,.2)
+        widget.pos_hint = {'x': .30, 'y': .30}
+        if fun: widget.bind(on_press=fun)
+        layout.add_widget(widget)
+        return widget
 
 ScannerApp().run()
