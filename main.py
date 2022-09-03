d1a06b92197a338ad53e76ae937109fdaeb30c9b
Aaron Kummer <aaronjkummer@gmail.com>
2022-09-03 13:49:16 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.camera import Camera
+from kivy.lang import Builder
+class ScannerApp(App):
+    def build(self):
+        print('test')
+        self.cam = Camera()
+        print('test2')
+        self.cam.play = True
+        self.cam.resolution = (800, 800)
+        button = Button(text="take picture")
+        button.size_hint = (.5,.2)
+        button.pos_hint = {'x': .25, 'y': .25}
+        button.bind(on_press=self.take_pic)
+        layout = BoxLayout()
+        layout.add_widget(self.cam)
+        layout.add_widget(button)
+        return layout
+
+    def take_pic(self, *args):
+        self.cam.export_to_png("./scan.png")
+    
+ScannerApp().run()
